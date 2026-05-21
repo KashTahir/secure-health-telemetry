bd2a2e63ec33e9a3a28bb59879300c5c3ce503d1
Kashmain Tahir <ktah0006@student.monash.edu>
2026-05-22 00:25:05 +0700

    feat: client-server sockets

--- client/patient_monitor.py
+++ client/patient_monitor.py
@@ -1,2 +1,29 @@
-print("Starting Patient Monitor...")
-print("Patient Monitor Started")
+import socket
+
+
+def connect_to_station():
+
+    try:
+
+        # set up
+        monitor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        monitor_socket.connect(('127.0.0.1', 9000))
+        print("CLIENT --Connected to server")
+
+        sample_data = """patient id: 100\nheart rate: 100\noxygen:100%"""
+        monitor_socket.sendall(sample_data.encode())
+        print("CLIENT -- data sent")
+
+        monitor_socket.close()
+
+
+    except Exception as error:
+        print(f"CLIENT -- could not start patient monitor: {error}")
+
+
+def main():
+    connect_to_station()
+
+
+if __name__ == "__main__":
+    main()
--- server/monitoring_station.py
+++ server/monitoring_station.py
@@ -1,2 +1,33 @@
-print("Starting Monitoring Station...")
-print("Monitoring Station Started")
+import socket
+
+
+def start_monitoring_station():
+
+    try:
+
+        # set up
+        station_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        station_socket.bind(('127.0.0.1', 9000))
+        station_socket.listen()
+        print("SERVER -- Monitoring Station waiting for incoming connections...")
+
+        # accept
+        connection_socket, connection_addr = station_socket.accept()
+        data = connection_socket.recv(1024)
+        print("SERVER -- data received")
+        print(data.decode())
+
+        connection_socket.close()
+        station_socket.close()
+
+
+    except Exception as error:
+        print(f"SERVER -- could not start monitoring station: {error}")
+
+
+def main():
+    start_monitoring_station()
+
+
+if __name__ == "__main__":
+    main()
